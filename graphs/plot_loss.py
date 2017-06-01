78fe4505895c8476b449161cff482423de3c951a
targil123 <rusakgili@gmail.com>
2017-06-01 04:34:52 +0000

    added newplotloss changes

--- graphs/plot_loss.py
+++ graphs/plot_loss.py
@@ -2,7 +2,7 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def plot_loss(filename, plot_title, max_val, x_scale):
+def plot_loss(filename, out_path, plot_title, max_val, x_scale):
     result = np.load(filename)
     train_loss = np.fmin(result['summary_train_loss'], max_val)
     eval_loss = np.fmin(result['summary_eval_loss'], max_val)
@@ -14,14 +14,15 @@ def plot_loss(filename, plot_title, max_val, x_scale):
     plt.xlabel('Iteration')
     plt.ylabel('Loss')
     plt.title(plot_title)
-    plt.show()
+    plt.savefig(out_path)
 
-if len(sys.argv) <= 1:
-    print("Usage: python plot_loss.py [filename] [title [max_val [x_scale]?]?]?")
+if len(sys.argv) <= 2:
+    print("Usage: python plot_loss.py [filename] [out_path] [title [max_val [x_scale]?]?]?")
 else:
     filename = sys.argv[1]
-    plot_title = sys.argv[2] if len(sys.argv) > 2 else 'Training and Validation Loss'
-    max_val = int(sys.argv[3]) if len(sys.argv) > 3 else 5000.0
-    x_scale = int(sys.argv[4]) if len(sys.argv) > 4 else 200
-    plot_loss(filename, plot_title, max_val, x_scale)
+    out_path = sys.argv[2]
+    plot_title = sys.argv[3] if len(sys.argv) > 3 else 'Training and Validation Loss'
+    max_val = int(sys.argv[4]) if len(sys.argv) > 4 else 5000.0
+    x_scale = int(sys.argv[5]) if len(sys.argv) > 5 else 200
+    plot_loss(filename, out_path, plot_title, max_val, x_scale)
     
