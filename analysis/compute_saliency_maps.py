8829b4eb5efad0459cd31edc3ba71f46884aeb79
Brad Ross <brad.ross.35@gmail.com>
2017-06-04 04:30:39 +0000

    fixed saliency map generation code

--- analysis/compute_saliency_maps.py
+++ analysis/compute_saliency_maps.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import os
 
 def load_model(sess, model_path, meta_graph):
@@ -24,43 +25,33 @@ def compute_saliency_maps_for(hists, labels, model_path, meta_graph):
         feed_dict = {x: hists, y: labels, keep_prob: 1}
         loss, hist_grad_val = sess.run([loss, hist_grad], feed_dict=feed_dict)
         rmse = np.sqrt(loss*2/hists.shape[0])
-        norm_hist_grad = hist_grad_val / np.amax(hist_grad_val, axis=(1, 2, 3)).reshape((-1, 1, 1, 1))
+        norm_hist_grad = hist_grad_val / np.amax(np.absolute(hist_grad_val), axis=(1, 2, 3)).reshape((-1, 1, 1, 1))
         return norm_hist_grad, rmse
 
 VAL_YEAR = 2013
+states_to_keep = np.array([5, 17, 18, 19, 20, 27, 29, 31, 38, 39, 46])
 
 # Soybean Saliency Maps
 soy_data = np.load(os.path.expanduser('~/cs231n-satellite-images-hist/data_soybean_filtered.npz'))
-index_validate = np.nonzero(soy_data['output_year'] == VAL_YEAR)[0]
 
-soy_sal_maps, soy_rmse = compute_saliency_maps_for(soy_data['output_image'][index_validate], soy_data['output_yield'][index_validate], os.path.expanduser('~/models/run0__dropout-0.25'), '2013CNN_model.ckpt.meta')
+hist_sums = np.sum(soy_data['output_image'],axis=(1,2,3))
+nonbroken_rows = hist_sums > 287
+imp_rows = pd.DataFrame(soy_data['output_index'])[0].isin(states_to_keep)
+val_year_rows = soy_data['output_year'] == VAL_YEAR
+print(np.sum(val_year_rows))
+index_validate = np.logical_and.reduce((nonbroken_rows, imp_rows, val_year_rows))
+print(np.sum(index_validate))
+soy_model_path = os.path.expanduser('~/models/run1__dropout_0.50__soybean')
+soy_sal_maps, soy_rmse = compute_saliency_maps_for(soy_data['output_image'][index_validate], soy_data['output_yield'][index_validate], soy_model_path, '2013CNN_model.ckpt.meta')
 print(soy_sal_maps.shape, soy_rmse)
 
 # Corn Saliency Maps
 corn_data = np.load(os.path.expanduser('~/cs231n-satellite-images-hist/data_corn.npz'))
-index_validate = np.nonzero(corn_data['output_year'] == VAL_YEAR)[0]
 
-corn_sal_maps, corn_rmse = compute_saliency_maps_for(corn_data['output_image'][index_validate], corn_data['output_yield'][index_validate], os.path.expanduser('~/models/run3__dropout-0.25__corn'), '2013CNN_model.ckpt.meta')
+corn_model_path = os.path.expanduser('~/models/run2__dropout_0.50__corn')
+corn_sal_maps, corn_rmse = compute_saliency_maps_for(corn_data['output_image'][index_validate], corn_data['output_yield'][index_validate], corn_model_path, 'important_counties2013CNN_model.ckpt.meta')
 print(corn_sal_maps.shape, corn_rmse)
 
 # Saving Saliency Maps
-np.savez(os.path.expanduser('~/cs231n-satellite-images-models/saliency_maps/original_model_comparison.npz'), soy_maps=soy_sal_maps, corn_maps=corn_sal_maps)
-
-# Comparing Soybean and Corn Saliency Maps
-def l2_diff(m1, m2):
-    return np.sqrt(np.mean((m1 - m2)**2))
-
-def l1_diff(m1, m2):
-    return np.mean(np.absolute(m1 - m2))
-
-rmse = l2_diff(corn_sal_maps, soy_sal_maps)
-abs_err = l1_diff(corn_sal_maps, soy_sal_maps)
-print(rmse, abs_err)
-
-temp_rmse = l2_diff(corn_sal_maps[:, :, 7:9], soy_sal_maps[:, :, 7:9])
-temp_abs_err = l1_diff(corn_sal_maps[:, :, 7:9], soy_sal_maps[:, :, 7:9])
-print(temp_rmse, temp_abs_err)
-
-spatial_rmse = l2_diff(corn_sal_maps[:, :, :7], soy_sal_maps[:, :, :7])
-spatial_abs_err = l1_diff(corn_sal_maps[:, :, :7], soy_sal_maps[:, :, :7])
-print(spatial_rmse, spatial_abs_err)
+output_path = os.path.expanduser('~/cs231n-satellite-images-models/saliency_maps/original_model_comparison_imp_count.npz')
+np.savez(output_path, soy_maps=soy_sal_maps, corn_maps=corn_sal_maps, index=soy_data['output_index'])
