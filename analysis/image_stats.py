077962a2fa377b531c4f9acadcdda57883e5c597
Mark Sabini <msabini@stanford.edu>
2017-06-04 05:55:07 +0000

    Add fixed image_stats and associated data to filter important counties.

--- analysis/image_stats.py
+++ analysis/image_stats.py
@@ -10,10 +10,13 @@ import gdal
 outpath = os.path.expanduser(sys.argv[1])
 MAX_FILES = int(sys.argv[2]) if len(sys.argv) > 2 else 1000000
 
+NUM_WORKERS = 4
+
 def gather_stats(pathname, raw_pathname, outpath, MAX_FILES):
     num_files = 0
     IMAGE_PATH = os.path.expanduser(pathname)
     RAW_PATH = os.path.expanduser(raw_pathname)
+    l_filenames = []
     heights = []
     widths = []
     depths = []
@@ -24,10 +27,17 @@ def gather_stats(pathname, raw_pathname, outpath, MAX_FILES):
         filenames_all = f.readlines()
     filenames = []
     for filename in filenames_all:
-        if 'zoom' not in filename:
-            filenames.append(filename)
+        if 'zoom' not in filename and filename.endswith('.npy\n'):
+            # print filename
+            if filename[23] == '_':
+                if int(filename[22:23]) in [5, 17, 18, 19, 20, 27, 29, 31, 38, 39, 46]:
+                    filenames.append(filename)
+            else:
+                if int(filename[22:24]) in [5, 17, 18, 19, 20, 27, 29, 31, 38, 39, 46]:
+                    filenames.append(filename)    
     if len(filenames) > MAX_FILES:
-        filename = list(np.random.choice(filenames, MAX_FILES, replace=False))
+        print 'Randomizing'
+        filenames = list(np.random.choice(filenames, MAX_FILES, replace=False))
     for filename in filenames:
         if num_files % 10 == 0:
             print "Iteration %d." % num_files 
@@ -35,6 +45,7 @@ def gather_stats(pathname, raw_pathname, outpath, MAX_FILES):
             break
         filename = filename.split('\n')[0]
         if filename.endswith('.npy'):
+            print filename
             num_files += 1
             arr = np.load(os.path.join(IMAGE_PATH, filename))
             arr = arr[:,:,54:342]
@@ -44,11 +55,13 @@ def gather_stats(pathname, raw_pathname, outpath, MAX_FILES):
             year = int(filename[17:21])
             raw_arr = raw_arr[:,:,322*(year-2003):322*(year-2003+1)]
             num_nonzero_pixels_raw = np.sum(raw_arr[:,:,0] > 0)
+            l_filenames.append(filename)
             heights.append(arr.shape[0])
             widths.append(arr.shape[1])
             aspect_ratios.append(arr.shape[1] / float(arr.shape[0]))
             num_nonzero_pixels = np.sum(arr > 0) / arr.shape[2]
             prop_nonzero_pixels.append(num_nonzero_pixels / float(num_nonzero_pixels_raw))
+            print prop_nonzero_pixels[-1]
             depths.append(arr.shape[2])
             sizes.append(os.path.getsize(os.path.join(IMAGE_PATH, filename)))
         np.savez(outpath,
@@ -57,7 +70,8 @@ def gather_stats(pathname, raw_pathname, outpath, MAX_FILES):
                  depths=np.array(depths),
                  aspect_ratios=np.array(aspect_ratios),
                  prop_nonzero_pixels=np.array(prop_nonzero_pixels),
-                 sizes=np.array(sizes))
+                 sizes=np.array(sizes),
+                 names=np.array(l_filenames))
             
 
 gather_stats('~/cs231n-satellite-images-clean', '~/cs231n-satellite-images', outpath, MAX_FILES)
