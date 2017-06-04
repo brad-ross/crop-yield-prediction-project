071c24cd7a506950001bb3284a26f1eb41e5bf3a
Brad Ross <brad.ross.35@gmail.com>
2017-06-04 04:52:44 +0000

    fixed output_rescaling

--- analysis/output_rescaling.py
+++ analysis/output_rescaling.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import pandas as pd
 import os
 
 def load_model(sess, model_path, meta_graph):
@@ -22,23 +23,30 @@ def predict_with_model(hists, model_path, meta_graph):
         feed_dict = {x: hists, keep_prob: 1}
         return sess.run(logits, feed_dict=feed_dict)
 
+VAL_YEAR = 2013
+states_to_keep = np.array([5, 17, 18, 19, 20, 27, 29, 31, 38, 39, 46])
+
 soy_data = np.load(os.path.expanduser('~/cs231n-satellite-images-hist/data_soybean_filtered.npz'))
-index_validate = np.nonzero(soy_data['output_year'] == 2013)[0]
+hist_sums = np.sum(soy_data['output_image'],axis=(1,2,3))
+nonbroken_rows = hist_sums > 287
+imp_rows = pd.DataFrame(soy_data['output_index'])[0].isin(states_to_keep)
+val_year_rows = soy_data['output_year'] == VAL_YEAR
+index_validate = np.logical_and.reduce((nonbroken_rows, imp_rows, val_year_rows))
+
 soy_yield = soy_data['output_yield'][index_validate]
 
 corn_data = np.load(os.path.expanduser('~/cs231n-satellite-images-hist/data_corn.npz'))
-index_validate = np.nonzero(corn_data['output_year'] == 2013)[0]
 corn_yield = corn_data['output_yield'][index_validate]
 
 # Scaling Soy Predictions to Corn
-corn_preds_w_soy_mod = predict_with_model(corn_data['output_image'][index_validate], os.path.expanduser('~/models/run0__dropout-0.25'), '2013CNN_model.ckpt.meta')
+corn_preds_w_soy_mod = predict_with_model(corn_data['output_image'][index_validate], os.path.expanduser('~/models/run1__dropout_0.50__soybean'), '2013CNN_model.ckpt.meta')
 std_corn_preds = (corn_preds_w_soy_mod - np.mean(soy_yield))/np.std(soy_yield)
 unstd_corn_preds = std_corn_preds * np.std(corn_yield) + np.mean(corn_yield)
 corn_rmse = np.sqrt(np.mean((unstd_corn_preds - corn_yield)**2))
 print(corn_rmse)
 
 # Scaling Corn Predictions to Soy
-soy_preds_w_corn_mod = predict_with_model(soy_data['output_image'][index_validate], os.path.expanduser('~/models/run3__dropout-0.25__corn'), '2013CNN_model.ckpt.meta')
+soy_preds_w_corn_mod = predict_with_model(soy_data['output_image'][index_validate], os.path.expanduser('~/models/run2__dropout_0.50__corn'), 'important_counties2013CNN_model.ckpt.meta')
 std_soy_preds = (soy_preds_w_corn_mod - np.mean(corn_yield))/np.std(corn_yield)
 unstd_soy_preds = std_soy_preds * np.std(soy_yield) + np.mean(soy_yield)
 soy_rmse = np.sqrt(np.mean((unstd_soy_preds - soy_yield)**2))
